4e73de2bae7acf4db26e9dacd0ce37099594c576
Garrick Larson Potts <ricki.glp@gmail.com>
2023-10-02 06:10:12 -0700

    Added the madatory tasks 0 - 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * check_cycle - Checks if the list has a cycle or not.
+ * @list: The list to be checked for a cycle.
+ * Return: 1 if there is a cycle or 0 if there is no cycle.
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *list_input = list, *list_copy = list;
+
+	if (list == NULL)
+		return (0);
+
+	while (list_input && list_copy)
+	{
+		list_input = list_input->next;
+		list_copy = list_copy->next->next;
+
+		if (list_input == list_copy)
+			return (1);
+	}
+	return (0);
+}
---
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+with open('z', 'r') as file:
+    z = file.read()
+    print(z)
