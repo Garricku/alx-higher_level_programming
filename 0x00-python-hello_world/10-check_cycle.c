849111beb16266a4612800e527d624daec8a0ad5
Garrick Larson Potts <ricki.glp@gmail.com>
2023-10-02 06:14:38 -0700

    corrections

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -9,9 +9,6 @@ int check_cycle(listint_t *list)
 {
 	listint_t *list_input = list, *list_copy = list;
 
-	if (list == NULL)
-		return (0);
-
 	while (list_input && list_copy)
 	{
 		list_input = list_input->next;
