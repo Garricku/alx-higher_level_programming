bfb91c06ed19a106d593449eb07091ded786032f
Garrick Larson Potts <ricki.glp@gmail.com>
2023-10-03 07:42:00 -0700

    task 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -3,4 +3,6 @@ def print_last_digit(number):
     last_digit = str(number)[-1]
     if (ord(last_digit)) >= ord('0') and (ord(last_digit)) <= ord('9'):
         print(last_digit, end='')
-    return last_digit
+        return last_digit
+    else:
+        raise ValueError("Invalid input")
