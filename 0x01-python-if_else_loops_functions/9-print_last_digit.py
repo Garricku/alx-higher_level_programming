5561ba6968690464f1e3d1da166aee7d93a81726
Garrick Larson Potts <ricki.glp@gmail.com>
2023-10-03 07:32:24 -0700

    task 9 corrections

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     last_digit = str(number)[-1]
-    print(last_digit, end='')
-    return last_digit
+    if (ord(last_digit)) >= ord('0') and (ord(last_digit)) <= ord('9'):
+        print(last_digit, end='')
+        return last_digit
