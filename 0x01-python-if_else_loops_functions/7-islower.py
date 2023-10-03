0d7070d337acbc0129a4d1caec60f7889aa306f0
Garrick Larson Potts <ricki.glp@gmail.com>
2023-10-03 09:12:41 -0700

    task 7

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -3,4 +3,7 @@ def islower(c):
     for i in range(ord('a'), ord('z') + 1):
         if c == chr(i):
             return True
-    return False
+    if c < ord('a') or c > ord('z') and c < ord('A') or c > ord('Z'):
+        raise ValueError("Invalid input")
+    else:
+        return False
