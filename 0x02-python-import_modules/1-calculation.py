197ba25641d697053f72d9b84f039b557ee77eee
Garrick Larson Potts <ricki.glp@gmail.com>
2023-10-05 09:58:11 -0700

    task 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
+import calculator_1
 if __name__ == "__main__":
     a = 10
     b = 5
-    print(f"{a:d} + {b:d} = {add(a, b):d}")
-    print(f"{a:d} - {b:d} = {sub(a, b):d}")
-    print(f"{a:d} * {b:d} = {mul(a, b):d}")
-    print(f"{a:d} / {b:d} = {div(a, b):d}")
+    print(f"{a:d} + {b:d} = {calculator_1.add(a, b):d}")
+    print(f"{a:d} - {b:d} = {calculator_1.sub(a, b):d}")
+    print(f"{a:d} * {b:d} = {calculator_1.mul(a, b):d}")
+    print(f"{a:d} / {b:d} = {calculator_1.div(a, b):d}")
