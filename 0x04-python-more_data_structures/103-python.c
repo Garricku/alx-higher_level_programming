7a77b0ff60fe58d7e1e1912ed1580d29d1cb6914
Garrick Larson Potts <ricki.glp@gmail.com>
2023-10-11 09:00:23 -0700

    tried some corrections

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,6 @@
+/**
+ * print_python_list - Print info on python.
+ * @p: Python object.
+ * Return: void.
+ */
+void print_python_list(PyObject *p);
--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    if set_1 and set_2:
         common_elements_set = set_1.intersection(set_2)
         return common_elements_set
