913d3ee44666929969e5ce532cb30f760dfa2d69
Garrick Larson Potts <ricki.glp@gmail.com>
2023-10-23 10:22:54 -0700

    task

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,11 +1,9 @@
 #!usr/bin/python3
 def safe_print_division(a, b):
-    result = None
     try:
-        if b != 0:
-            result = a/b
+        result = a/b
     except ZeroDivisionError:
-        raise ZeroDivisionError
+        result = None
     finally:
         print("Inside result: {}".format(result))
         return result
