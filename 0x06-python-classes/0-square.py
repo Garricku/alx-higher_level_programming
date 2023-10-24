483c82862deff3b1d66ad755c7a4e7386d91d456
Garrick Larson Potts <ricki.glp@gmail.com>
2023-10-24 04:28:15 -0700

    task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
 class Square:
+    """Documentation for Square.
+
+    This is for the information on the square and how it works, however this 
+    is a test file therefore the square class is empty.
+
+    Square:
+        The square is just an empty test square and this documentation is to 
+        practise creating documentation for classes::
+
+    This is the end of the documentation.
+    """
     pass
