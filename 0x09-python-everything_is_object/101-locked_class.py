52ca188df89f077a815f6c7cff26523fc9546d10
Garrick Larson Potts <ricki.glp@gmail.com>
2023-10-31 08:01:57 -0700

    task 101

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,2 @@
-def init(self, first_name):
-    self.first_name = first_name
-
-LockedClass = type("LockedClass", (), {"__slots__": ["first_name"],
-    "__init__": init})
+class LockedClass:
+    __slots__ = ['first_name']
