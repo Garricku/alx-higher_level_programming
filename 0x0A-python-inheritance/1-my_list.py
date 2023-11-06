08ebb021041c51bc7dae5b469ff0f4f80719a68b
Garrick Larson Potts <ricki.glp@gmail.com>
2023-11-06 06:10:54 -0800

    test file 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -8,3 +8,8 @@ class Mylist(list):
     """Prints sorted information of the class Mylist."""
 
     print(sorted(self))
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/1-my_list.txt")
