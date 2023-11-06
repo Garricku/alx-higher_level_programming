d12199c96428c1b85aa2cf66fa9d3f3a5c509f36
Garrick Larson Potts <ricki.glp@gmail.com>
2023-11-06 05:21:14 -0800

    task 1

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This is for the module showing inheritence."""
+
+
+def lookup(obj):
+    """Returns the list of available attributes and methods of an object."""
+
+    return dir(obj)
