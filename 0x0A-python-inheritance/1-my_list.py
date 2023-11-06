7ba5eb96b9619cfffe50b2397c9e43b417ba0e1b
Garrick Larson Potts <ricki.glp@gmail.com>
2023-11-06 06:52:26 -0800

    task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """This module is for the lists class."""
 
-class Mylist(list):
-    """Defines a class called MYlist that inherits from list."""
+class MyList(list):
+    """Defines a class called MyList that inherits from list."""
 
     def print_sorted(self):
-    """Prints sorted information of the class Mylist."""
+        """Prints sorted information of the class Mylist."""
 
-    print(sorted(self))
+        print(sorted(self))
 
 
 if __name__ == "__main__":
