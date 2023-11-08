1bf0209724ba4dd6ba4f92d691ebda7e22fc0618
Garrick Larson Potts <ricki.glp@gmail.com>
2023-11-08 08:31:59 -0800

    advanced task 100

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -12,4 +12,4 @@ def append_after(filename="", search_string="", new_string=""):
         for line in lines:
             my_file.write(line)
             if search_string in line:
-                my_file.write(new_string + '\n')
+                my_file.write(new_string)
