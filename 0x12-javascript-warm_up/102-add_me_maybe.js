446231591cad002eeecdc4833127c42024718f81
Garrick Larson Potts <ricki.glp@gmail.com>
2024-01-08 10:41:41 -0800

    adv task 102

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 // Write a function that increments and calls a function.
 function addMeMaybe (number, theFunction) {
-  for (let count = 0; count < number; count++) {
-    theFunction();
-  }
+  number++;
+  theFunction(number);
 }
 
 module.exports = { addMeMaybe };
