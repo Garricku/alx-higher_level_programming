8fa83de125669e64cbde93098520673f0d7666fe
Garrick Larson Potts <ricki.glp@gmail.com>
2024-01-08 04:37:37 -0800

    corrected a A to an a in the word Argument - argument

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -7,7 +7,7 @@
 const { argv } = require('node:process');
 
 if (argv.length <= 2) {
-  console.log('No Argument');
+  console.log('No argument');
 } else if (argv.length === 3) {
   console.log('Argument found');
 } else {
