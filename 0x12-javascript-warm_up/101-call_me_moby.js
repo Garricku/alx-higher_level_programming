b5e55980a5816103b81fc05a7ca9ec0844bc4450
Garrick Larson Potts <ricki.glp@gmail.com>
2024-01-08 09:23:29 -0800

    task adv 101

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -2,14 +2,9 @@
 // Write a function that executes x times a function.
 
 function callMeMoby (x, theFunction) {
-  let count = 0;
-  const intervalId = setInterval(() => {
+  for (let count = 0; count < x; count++) {
     theFunction();
-    count++;
-    if (count === x) {
-      clearInterval(intervalId);
-    }
-  }, 100);
+  }
 }
 
 module.exports = { callMeMoby };
