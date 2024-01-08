21d03f78016340533e6fe10dc97a56d916e001da
Garrick Larson Potts <ricki.glp@gmail.com>
2024-01-08 05:34:10 -0800

    task 5 standard corrections

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
-/* 
- * Write a script that prints My number: <first argument converted in integer>
+/* Write a script that prints My number: <first argument converted in integer>
  * if the first argument can be converted to an integer.
  * If the argument can’t be converted to an integer, print “Not a number”
  */
 const firstArg = parseInt(process.argv[2]);
 const numErr = 'Not a number';
-const message = 'My number: '
+const message = 'My number: ';
 
 if (isNaN(firstArg)) {
   console.log(numErr);
