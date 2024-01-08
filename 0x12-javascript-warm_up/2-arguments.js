6ac4c111a187030ab55bae558440d1e4dc3a7ed8
Garrick Larson Potts <ricki.glp@gmail.com>
2024-01-08 04:35:05 -0800

    task 2 semistandard corrections

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -7,9 +7,9 @@
 const { argv } = require('node:process');
 
 if (argv.length <= 2) {
-	console.log('No Argument');
+  console.log('No Argument');
 } else if (argv.length === 3) {
-	console.log('Argument found');
+  console.log('Argument found');
 } else {
-	console.log('Arguments found');
+  console.log('Arguments found');
 }
