55c1fd10665fb8b80991b1326fa7be697133cd72
Garrick Larson Potts <ricki.glp@gmail.com>
2024-01-12 12:43:45 -0800

    task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
             )
 
     cur = db.cursor()
-    cur.execute("""SELECT * FROM cities ORDER BY id ASC""")
+    cur.execute("SELECT * FROM cities ORDER BY id ASC")
 
     for row in cur.fetchall():
         print(row)
