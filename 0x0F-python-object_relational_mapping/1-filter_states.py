a73c09367c696241847eda4886f839303e0db24b
Garrick Larson Potts <ricki.glp@gmail.com>
2024-01-12 01:56:10 -0800

    task 1 trying to fix, some checks are failing

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """
-Defines the module 1-filter_states.py
+Defines the module 1-filter_states
+Write a script that lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa
 """
 
 
@@ -12,18 +14,20 @@ Defines the imported modules
 
 
 if __name__ == "__main__":
-    # Connects to the MySQL server running on localhost at port 3306
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+            )
 
-    # Executes the SQL query to retrieve all states starting with N
+    sql = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC"
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute(sql)
 
-    # Prints the results in the format specified in the prompt
     for row in cur.fetchall():
         print(row)
 
-    # Closes the cursor and database connection
     cur.close()
     db.close()
