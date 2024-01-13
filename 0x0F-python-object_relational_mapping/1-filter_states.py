ac7a7c63a1a81a7ecab64f8d9267dcd6b9903fe3
Garrick Larson Potts <ricki.glp@gmail.com>
2024-01-13 06:29:26 -0800

    task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -22,9 +22,14 @@ if __name__ == "__main__":
             db=sys.argv[3]
             )
 
-    s = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC"
+    "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY states.id ASC"
     cur = db.cursor()
-    cur.execute(s)
+    cur.execute("""
+                SELECT *
+                FROM states
+                WHERE name LIKE BINARY 'N%'
+                ORDER BY states.id ASC"""
+                )
 
     for row in cur.fetchall():
         print("({}, '{}')".format(row[0], row[1]))
