3e401c3c5209dd07e2aa3f25a9f567ef7e4409f0
Garrick Larson Potts <ricki.glp@gmail.com>
2024-01-25 06:12:16 -0800

    task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL, sends a GET request to the URL, and displays the body
-response=$(curl -s -w "%{http_code}"-X GET $1 -o /dev/null); echo "$response"
+response=$(curl -s -w "%{http_code}" -X GET $1 -o /dev/null); echo "$response"
