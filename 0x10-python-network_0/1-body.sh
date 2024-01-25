b5c2c1fe7e27733043db935bd424014be46c7c20
Garrick Larson Potts <ricki.glp@gmail.com>
2024-01-25 06:41:58 -0800

    task 1 and 2

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL, sends a GET request to the URL, and displays the body
-curl -s -w "%{http_code}" -X GET $1 | grep -q 200 && curl -s -X GET $1 | tail -n +11; echo -n 'Route 2'
+curl -s -w "%{http_code}" -X GET $1 | grep -q 200 && curl -s -X GET $1 | tail -n +11
--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Write a Bash script that sends a DELETE request to the URL
-curl -s -X DELETE $1; echo -n "I'm a DELETE request"
+curl -s -X DELETE $1;
