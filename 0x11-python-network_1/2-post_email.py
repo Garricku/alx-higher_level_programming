3f6ca9c729e445c73555d868cb8297354e4fbca1
Garrick Larson Potts <ricki.glp@gmail.com>
2024-01-26 06:03:41 -0800

    task 2 completed POST an email #0, edited for pycodstly added a whitespace after a ,

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
     data = urllib.parse.urlencode({'email': email}).encode('utf-8')
-    req = urllib.request.Request(url,data)
+    req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
         print(response.read().decode('utf-8'))
