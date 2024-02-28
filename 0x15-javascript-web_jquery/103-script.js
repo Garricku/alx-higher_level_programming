71565145593403ba195e2de7c5dbf7f4c5205c25
Garrick Larson Potts <ricki.glp@gmail.com>
2024-02-28 03:59:43 -0800

    semistandard

--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   const languageInput = $('#language_code');
-  const translateButton = $("#btn_translate");
-  const helloDiv = $("#hello");
+  const translateButton = $('#btn_translate');
+  const helloDiv = $('#hello');
   translateButton.click(function () {
     const languageCode = languageInput.val();
     $.get(`https://www.fourtonfish.com/hellosalut/hello/?lang=${languageCode}`, function (data) {
